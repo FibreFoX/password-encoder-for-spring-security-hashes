1b693b613573349f8b94fad928b957d43f917633
FibreFoX <fibrefox@dynamicfiles.de>
2023-09-01 20:22:31 +0200

    some preparations

--- src/encoder/standard.rs
+++ src/encoder/standard.rs
@@ -1,3 +1,13 @@
+#[derive(Debug)]
 pub struct StandardPasswordEncoder {
-    secret: Option<String>,
+    iterations: u32,
+}
+
+impl Default for StandardPasswordEncoder {
+    fn default() -> StandardPasswordEncoder {
+        StandardPasswordEncoder {
+            // https://github.com/spring-projects/spring-security/blob/0e3ee8990299436223c3037afb096f454f17ef5e/crypto/src/main/java/org/springframework/security/crypto/password/StandardPasswordEncoder.java#LL53C48-L53C52
+            iterations: 1024
+        }
+    }
 }
